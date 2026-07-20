68a953eb217a64c9420b727bfec1bed8e57b538b
redlaser42 <120990408+redlaser42@users.noreply.github.com>
2026-07-20 19:52:36 -0400

    Closing menu next frame with null check

--- Patches/PlayerApplyDamageInfo_Patch.cs
+++ Patches/PlayerApplyDamageInfo_Patch.cs
@@ -1,6 +1,8 @@
 using EFT;
+using EFT.UI;
 using HarmonyLib;
 using SPT.Reflection.Patching;
+using System.Collections;
 using System.Reflection;
 
 namespace CloseMenuOnDamage.Patches
@@ -15,15 +17,16 @@ namespace CloseMenuOnDamage.Patches
         [PatchPostfix]
         static void Postfix(Player __instance, DamageInfoStruct damageInfo)
         {
-            bool isBulletDamage = (damageInfo.DamageType == EDamageType.Bullet);
-            if (isBulletDamage)
-            {
-                if (InventoryScreenAwake_Patch.inventoryScreen != null && __instance.IsYourPlayer && Plugin.Enabled.Value)
-                {
-                    InventoryScreenAwake_Patch.inventoryScreen.method_8();
-                    Plugin.CloseBepInExConfig();
-                }
-            }
+            CloseNextFrame();
+        }
+
+
+        private static IEnumerator CloseNextFrame()
+        {
+            yield return null;
+
+            var screen = InventoryScreenAwake_Patch.inventoryScreen;
+            screen?.method_8();
         }
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -1,10 +1,10 @@
 ﻿using BepInEx;
 using BepInEx.Bootstrap;
 using BepInEx.Configuration;
-using BepInEx.Logging;
 using CloseMenuOnDamage.Patches;
+using HarmonyLib;
+using System;
 using System.Reflection;
-using UnityEngine;
 
 namespace CloseMenuOnDamage
 {
@@ -13,39 +13,12 @@ namespace CloseMenuOnDamage
     {
         public static ConfigEntry<bool>? Enabled;
 
-        public static BaseUnityPlugin? BepInExConfigManager = null;
-
         private void Awake()
         {
         Enabled = Config.Bind("General", "Enabled", true, "Activates the plugin.");
 
         new InventoryScreenAwake_Patch().Enable();
         new PlayerApplyDamageInfo_Patch().Enable();
-
-        var configManagerInfo = Chainloader.PluginInfos.TryGetValue(
-        "com.bepis.bepinex.configurationmanager",
-        out PluginInfo pluginInfo);
-
-        if (configManagerInfo)
-        {
-            BepInExConfigManager = pluginInfo.Instance;
-            MethodInfo displayMethod = BepInExConfigManager.GetType().GetMethod(
-                "ToggleWindow",
-                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-
-            displayMethod?.Invoke(BepInExConfigManager, null);
-        }
-
-        }
-        public static void CloseBepInExConfig()
-        {
-            var prop = BepInExConfigManager.GetType().GetProperty("DisplayingWindow",BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-
-            if (prop != null && (bool)prop.GetValue(BepInExConfigManager))
-            {
-                prop.SetValue(BepInExConfigManager, false);
-                return;
-            }
         }
     }   
 }
